4848882931906ad7eafcd925cf7c229677df7dca
8char <matteokrantz@gmail.com>
2022-04-30 17:01:53 +0200

    🔨 Added warning messages incase of a bad config

--- src/Bot.ts
+++ src/Bot.ts
@@ -3,13 +3,22 @@ require("dotenv").config();
 import eventLoader from "./EventLoader";
 
 console.log("⌛ Bot is starting...");
-console.log(`🐛 Debug level: ${process.env.NODE_ENV?.toLocaleUpperCase() || "NONE"}`)
+if (!process.env.CLIENT_TOKEN) {
+    console.error("❌ You don't have a token set up, please do so in the .env file!");
+    process.exit();
+}
+if(!process.env.NODE_ENV) {
+    console.warn("⚠️ We recommend that you run the bot using the predetermined yarn scripts, you may run in to issues if you don't!")
+    process.env.NODE_ENV = "NONE";
+}
+console.log(`🐛 Debug level: ${process.env.NODE_ENV.toLocaleUpperCase()}`)
 
 //Enables the required intents for the bot.
 const client = new Client({
     intents: [
         Intents.FLAGS.GUILDS,
-        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_PRESENCES,
         Intents.FLAGS.DIRECT_MESSAGES,
     ],
 });
--- src/listeners/ready.ts
+++ src/listeners/ready.ts
@@ -1,5 +1,6 @@
-import { Client } from "discord.js";
+import { Client, MessageEmbed} from "discord.js";
 import Commands from "../Commands";
+require("dotenv").config(`${__dirname}/../.env`);
 
 export default (client: Client): void => {
     client.on("ready", async () => {
@@ -10,5 +11,14 @@ export default (client: Client): void => {
         await client.application.commands.set(Commands);
 
         console.log(`✔️  ${client.user.tag} is online`);
+        if (!process.env.LOGGING_CHANNEL) return console.warn("⚠️ We suggest that you set up a logging channel id in the .env file!");
+        const channel = await client.channels.cache.get(process.env.LOGGING_CHANNEL);
+        const logEmbed = new MessageEmbed()
+            .setTitle("🟢 __**I'm live!**__")
+            .setDescription(`*Bot has been started at ${new Date()}*`)
+            .setTimestamp()
+            .setColor("#32CD32")
+        if (channel?.isText())
+            channel.send({embeds: [logEmbed]});
     });
 };
